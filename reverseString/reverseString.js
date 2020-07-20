22152cebf67f945889ad5eacc46f71a8e943d218
samuel199two <ekatisamu@gmail.com>
2020-07-20 18:46:12 +0300

    Solved reverseStringProbem

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
+    
 }
 
-module.exports = reverseString
+module.exports = reverseString;
+
